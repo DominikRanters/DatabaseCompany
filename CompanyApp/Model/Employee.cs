aaabc60609a84525d385a4891a8dd805b292d108
Dominik Ranters <dominik.ranters@tobit.cm>
2019-09-11 12:26:33 +0200

    :rocket: Add employee model

--- CompanyApp/Model/Employee.cs
+++ CompanyApp/Model/Employee.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace CompanyApp.Model
 {
-    class Employee
+    public class Employee
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int DepartmentId { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Street { get; set; }
+        public string Country { get; set; }
     }
 }
