3c2fd139fbe8ad22ef6ef6e5e3a39cbdfd6e30c4
Dominik Ranters <dominik.ranters@tobit.cm>
2019-09-13 12:32:20 +0200

    :rocket: Create Folder

---
+++ CompanyAPI/Controllers/CompanyController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using CompanyAPI.Interface;
+using CompanyAPI.Model;
+using CompanyAPI.Model.Dto;
+using CompanyAPI.Repository;
+
+
+namespace CompanyAPI.Controller
+{
+    [Route("company")]
+    public class CompanyController : ControllerBase
+    {
+        readonly IBaseInterface<Company> _companyRepository;
+
+        public CompanyController()
+        {
+            _companyRepository = new CompanyRepository("Data Source=tappqa;Initial Catalog=Training-DS-Company;Integrated Security=True");
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            return Ok(_companyRepository.Read());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetCompany(int id)
+        {
+            return Ok(_companyRepository.Read(id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult PostCompany([FromBody] CompanyDto company)
+        {
+            return NoContent;
+        }
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public IActionResult PutCompany(int id, [FromBody] string value)
+        //{
+        //    return
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public IActionResult DeleteCompany(int id)
+        //{
+        //    return
+        //}
+    }
+}
