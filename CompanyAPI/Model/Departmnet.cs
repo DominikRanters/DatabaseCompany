922aa74e7db638666277f5bb3be7a8bbcdb19605
Dominik Ranters <dominik.ranters@tobit.cm>
2019-09-17 09:16:25 +0200

    :rocket: Add department models

--- CompanyAPI/Model/Departmnet.cs
+++ CompanyAPI/Model/Departmnet.cs
@@ -7,5 +7,9 @@ namespace CompanyAPI.Model
 {
     public class Departmnet
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CompanyId { get; set; }
     }
 }
--- CompanyAPI/Model/Dto/DepartmentDto.cs
+++ CompanyAPI/Model/Dto/DepartmentDto.cs
@@ -7,5 +7,8 @@ namespace CompanyAPI.Model.Dto
 {
     public class DepartmentDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CompanyId { get; set; }
     }
 }
